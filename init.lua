20d43c524c627d8b776c5ae9aa4d286349a2f64b
Cka3kaY <elysyar@gmail.com>
2025-02-25 22:34:23 +0100

    yes we are back

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
 require("config.lazy")
 
-
--- Theme
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-
 -- Telescope keybindings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
@@ -12,13 +7,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
--- make nvim transparent
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
 
 -- autoclose setup
 require("autoclose").setup()
@@ -27,6 +15,32 @@ require("autoclose").setup()
 require('lualine').setup()
 
 -- general settings
+vim.keymap.set("n", "<leader>e", ":Ex<CR>", {})
+
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+})
+-- make nvim transparent
+--
+-- Theme
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.keymap.set("n", "<leader>ee", ":Ex<CR>", {})
+vim.api.nvim_set_option("clipboard", "unnamed")
